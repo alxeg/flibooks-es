94686e748bb687a5f094add2f47adb37c1b19937
alxeg <alx.egorov@gmail.com>
2018-10-08 17:12:40 +0300

    Add listing of the author's books.

--- src/serve/mod.rs
+++ src/serve/mod.rs
@@ -18,20 +18,7 @@ pub mod request;
 const MAX_BODY_LENGTH: usize = 1024 * 1024 * 10;
 
 lazy_static! {
-    pub static ref ES: SyncClient = connect_es().unwrap();
-}
-
-pub fn connect_es() -> Result<SyncClient, Box<Error>> {
-    match conf::SETTINGS.read() {
-        Ok(settings) => {
-            let base_url = settings.elastic_base_url.as_str();
-
-            info!("Using the elasticsearch at '{}'", base_url);
-
-            Ok(SyncClientBuilder::new().base_url(base_url).build()?)
-        }
-        _ => Err(From::from("Failed to read settings")),
-    }
+    pub static ref ES: SyncClient = es_connect().unwrap();
 }
 
 pub fn start() -> Result<(), Box<Error>> {
@@ -40,7 +27,8 @@ pub fn start() -> Result<(), Box<Error>> {
     let listen = &settings.listen_address;
 
     let router = router!{
-        authors: post "/api/author/search" => authors_handler,
+        authr:       post "/api/author/search" => authors_handler,
+        authr_books: post "/api/author/books"  => authors_books_handler,
     };
 
     let mut chain = Chain::new(router);
@@ -54,15 +42,52 @@ pub fn start() -> Result<(), Box<Error>> {
     Ok(())
 }
 
+pub fn es_connect() -> Result<SyncClient, Box<Error>> {
+    match conf::SETTINGS.read() {
+        Ok(settings) => {
+            let base_url = settings.elastic_base_url.as_str();
+
+            info!("Using the elasticsearch at '{}'", base_url);
+
+            Ok(SyncClientBuilder::new().base_url(base_url).build()?)
+        }
+        _ => Err(From::from("Failed to read settings")),
+    }
+}
+
+fn es_search(query: Value, filter: &str) -> Result<String, Box<Error>> {
+    let mut result = String::new();
+
+    let req = {
+        let body = query;
+        SearchRequest::new(body)
+    };
+
+    let mut raw = String::new();
+    ES.request(req).send()?.into_raw().read_to_string(&mut raw)?;
+    let json: Value = serde_json::from_str(raw.as_str())?;
+
+    let selector = Selector::new(filter)?;
+    let mut found = selector.find(&json);
+    match found.next() {
+        Some(filtered) => {
+            result.push_str(&serde_json::to_string(&(*filtered))?.to_string());
+        }
+        None => return Err(From::from("No matched data found")),
+    }
+
+    Ok(result)
+}
+
 fn authors_handler(req: &mut Request) -> IronResult<Response> {
     let body = req.get::<bodyparser::Struct<request::Author>>();
-    let mut _error_response = Response::with((status::BadRequest, "Internal Server Error"));
+    let mut _error_response = Response::with((status::BadRequest, "Server Error"));
 
     match body {
         Ok(Some(author_req)) => {
-            debug!("Parsed author request:\n{:?}", author_req);
+            debug!("Search author request:\n{:?}", author_req);
 
-            match search_es(
+            match es_search(
                 json!({
                     "size": 0,
                     "aggs": {
@@ -93,26 +118,67 @@ fn authors_handler(req: &mut Request) -> IronResult<Response> {
     Ok(_error_response)
 }
 
-fn search_es(query: Value, filter: &str) -> Result<String, Box<Error>> {
-    let mut result = String::new();
+fn authors_books_handler(req: &mut Request) -> IronResult<Response> {
+    let mut _error_response = Response::with((status::BadRequest, "Server Error"));
+    let body = req.get::<bodyparser::Struct<request::Search>>();
 
-    let req = {
-        let body = query;
-        SearchRequest::new(body)
-    };
+    match body {
+        Ok(Some(search)) => {
+            debug!("Author's books request:\n{:?}", search);
+            let del = if search.deleted { 1 } else { 0 };
 
-    let mut raw = String::new();
-    ES.request(req).send()?.into_raw().read_to_string(&mut raw)?;
-    let json: Value = serde_json::from_str(raw.as_str())?;
+            if !search.author.is_empty() {
+                let mut req = json!({
+                    "size": search.limit,
+                    "query": {
+                        "bool": {
+                            "filter": [{
+                                    "match_phrase_prefix": {
+                                        "authors": search.author
+                                    }
+                                }, {
+                                    "terms": {
+                                        "del": [ 0, del ]
+                                    }
+                            }]
+                        }
+                }});
 
-    let selector = Selector::new(filter)?;
-    let mut found = selector.find(&json);
-    match found.next() {
-        Some(filtered) => {
-            result.push_str(&serde_json::to_string(&(*filtered))?.to_string());
+                if !search.langs.is_empty() {
+                    req["query"].as_object_mut().unwrap()["bool"]
+                        .as_object_mut()
+                        .unwrap()["filter"]
+                        .as_array_mut()
+                        .unwrap()
+                        .push(json!(
+                        {
+                            "terms": {
+                                "lang": search.langs
+                            }
+                        }
+                    ));
+                }
+
+                match es_search(req, "$.hits.hits") {
+                    Ok(search_result) => {
+                        return Ok(Response::with((
+                            status::Ok,
+                            Header(headers::ContentType::json()),
+                            search_result,
+                        )));
+                    }
+                    Err(e) => _error_response = Response::with((status::NotFound, e.to_string())),
+                }
+            } else {
+                _error_response =
+                    Response::with((status::BadRequest, "No author in search request"));
+            }
         }
-        None => return Err(From::from("No matched data found")),
+        _ => {
+            _error_response = Response::with((status::BadRequest, "Failed to parse search request"))
+        }
     }
 
-    Ok(result)
+    error!("Responding with error: {}", _error_response);
+    Ok(_error_response)
 }
--- src/serve/request.rs
+++ src/serve/request.rs
@@ -5,6 +5,34 @@ pub struct Author {
     pub limit: u32,
 }
 
+#[derive(Debug, Deserialize, Clone)]
+pub struct Search {
+    #[serde(default = "default_empty_string")]
+    pub title: String,
+    #[serde(default = "default_empty_string")]
+    pub author: String,
+    #[serde(default = "default_empty_string")]
+    pub series: String,
+    #[serde(default = "default_limit")]
+    pub limit: u32,
+    #[serde(default = "default_deleted")]
+    pub deleted: bool,
+    #[serde(default = "default_langs")]
+    pub langs: Vec<String>,
+}
+
 pub fn default_limit() -> u32 {
     10
+}
+
+pub fn default_deleted() -> bool {
+    false
+}
+
+pub fn default_empty_string() -> String {
+    "".to_string()
+}
+
+pub fn default_langs() -> Vec<String> {
+    Vec::new()
 }
